ec274adf1e240de7e6c0d3387623a7dddd87ecdd
Christoph Witzko <github@christophwitzko.com>
2022-05-23 16:49:55 +0200

    feat: detect homepage and repository from github action environment variables

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func mustGetString(cmd *cobra.Command, name string) string {
 	res, err := cmd.Flags().GetString(name)
@@ -34,7 +39,17 @@ type Config struct {
 	NoPrefixForMainPackage bool
 }
 
+func getRepositoryFromEnv() (string, string) {
+	serverUrl := os.Getenv("GITHUB_SERVER_URL")
+	repo := os.Getenv("GITHUB_REPOSITORY")
+	if serverUrl == "" || repo == "" {
+		return "", ""
+	}
+	return fmt.Sprintf("github:%s", repo), fmt.Sprintf("%s/%s", serverUrl, repo)
+}
+
 func InitConfig(cmd *cobra.Command) {
+	repo, homepage := getRepositoryFromEnv()
 	cmd.Flags().StringP("input-path", "i", "./bin", "input path that contains the binary files")
 	cmd.Flags().StringP("output-path", "o", "./generated-packages", "output directory")
 	cmd.Flags().StringP("name", "n", "", "name of the binary (e.g my-cool-cli)")
@@ -42,9 +57,9 @@ func InitConfig(cmd *cobra.Command) {
 	cmd.Flags().StringP("package-version", "r", "", "version of the created packages")
 	cmd.Flags().String("package-name", "", "package name [defaults to 'name'] (e.g. my-cool-cli)")
 	cmd.Flags().String("license", "", "package SPDX license (e.g. MIT)")
-	cmd.Flags().String("homepage", "", "package homepage")
+	cmd.Flags().String("homepage", homepage, "package homepage")
 	cmd.Flags().String("description", "", "package description")
-	cmd.Flags().String("repository", "", "package repository")
+	cmd.Flags().String("repository", repo, "package repository")
 	cmd.Flags().String("publish-registry", "https://registry.npmjs.org/", "npm registry endpoint")
 	cmd.Flags().Bool("publish", false, "run npm publish for all packages")
 	cmd.Flags().Bool("no-prefix-for-main-package", false, "ignore the configured package name prefix for the main package")
